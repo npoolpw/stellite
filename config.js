5bd92713d4f8a83128ffa34bab65f3be402dd3a6
npool.pw <36919430+npoolpw@users.noreply.github.com>
2018-03-01 08:07:42 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+var api = "http://10.25.202.224:2000";
+
+var poolHost = "10.25.202.224";
+
+var blockExplorer = "http://blockexplorer.pw/africoin";
+
+var cryptonatorWidget = ["BTC-USD", "XMR-USD"];
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "http://blockexplorer.pw/{symbol}/?hash={id}#blockchain_block";
+
+var transactionExplorer = "http://blockexplorer.pw/{symbol}/?hash={id}#blockchain_transaction";
+
+var themeCss = "themes/default-theme.css";
+
+var networkStat = [
+        ["mym.npool.pw", "http://mym.npool.pw:10070"],
+	["bitnotepool.ga/mymn", "http://35.156.246.254:9001"]
+];
