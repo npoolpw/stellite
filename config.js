c4dcc12715861752a3137fdf0000883632bd448a
npool.pw <36919430+npoolpw@users.noreply.github.com>
2018-03-01 08:09:32 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-var api = "http://10.25.202.224:2000";
+var api = "stl.npool.pw:8113;
 
-var poolHost = "10.25.202.224";
+var poolHost = "1stl.npool.pw";
 
 var blockExplorer = "http://blockexplorer.pw/africoin";
 
@@ -15,6 +15,13 @@ var transactionExplorer = "http://blockexplorer.pw/{symbol}/?hash={id}#blockchai
 var themeCss = "themes/default-theme.css";
 
 var networkStat = [
-        ["mym.npool.pw", "http://mym.npool.pw:10070"],
-	["bitnotepool.ga/mymn", "http://35.156.246.254:9001"]
+        ["stl.bfg.rocks", "https://stl.bfg.rocks:8117"],
+        ["stl.poolmine.ovh", "https://stl.poolmine.ovh:2443"],
+        ["stl.cryptonight.today", "http://172.110.22.125:8117"],
+        ["cryptonote.1234pools.com/stellite", "5.135.197.249:8117"],
+        ["www.stellitepool.com", "http://www.stellitepool.com:8117"],
+        ["stl.hpool.net", "http://stl.hpool.net:8126"],
+        ["stl.dpool.cc", "http://stl.dpool.cc:3004"],
+        ["cryptoknight.cc/stellite", "http://78.46.85.142:2889"],
+        ["npool.pw/stellite", "http://stl.npool.pw:8113"]
 ];
